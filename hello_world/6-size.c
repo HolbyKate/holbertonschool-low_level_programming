62ba714ed021c6a78a97275d8926e47954067beb
root <you@example.com>
2023-06-16 07:00:05 -0700

    sizelu

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  * Return: Always (Success)
  */
 int main(void)
-{printf("Size of a char: %lu bytes\n", sizeof(charType));
-printf("Size of an int: %lu bytes\n", sizeof(intType));
-printf("Size of a long int: %lu bytes\n", sizeof(longintType));
-printf("Size of a long long int: %lu bytes\n", sizeof(longlongintType));
-printf("Size of a float: %lu bytes\n", sizeof(floatType));
+{printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
