316d4084da1ba6baa1333232a1d336a1f15cf9dc
root <you@example.com>
2023-06-19 15:06:10 -0700

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *  main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++ \n)
+
+
+		putchar (ch);
+
+	return (0);
+}
