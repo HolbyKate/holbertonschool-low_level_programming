ecb87c53af9741924576a4be20a41e28877c9fdf
root <you@example.com>
2023-06-19 06:13:08 -0700

    if!felseelse

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,16 +11,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-	{
-		printf("is positive"\n);
-	}
+		puts("is positive");
 	else if (n == 0)
-	{
-		printf("is zero"\n);
-	}
+		puts("is zero");
+
 	else
-	{
-		printf("is negative"\n);
-	}
+		puts("is negative");
 	return (0);
 }
