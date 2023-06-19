874d5c77c50a8b677a3f1b69802142b073e3e3e9
root <you@example.com>
2023-06-19 04:02:03 -0700

    ififelseelse

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,14 +10,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0){
-		printf("is positive");
+	if (n > 0)
+	{
+		printf("is positive"\n);
 	}
-	else if (n == 0){
-		printf("is zero");
+	else if (n == 0)
+	{
+		printf("is zero"\n);
 	}
-	else (n < 0){
-		printf("is negative");
+	else
+	{
+		printf("is negative"\n);
 	}
 	return (0);
 }
