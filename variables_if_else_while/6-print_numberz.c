52f4999180c9cf74575bae7ac581e1eb605f9591
root <you@example.com>
2023-06-20 12:21:43 -0700

    digit putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (num = 0; num < 10; num++)
+	{       putchar(num);
+	}
+
+	{ putchar("\n");
+	}
+	return (0);}
