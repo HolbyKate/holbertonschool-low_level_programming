4867099638446b6d2cb2bc3271b793bbec32ae11
root <you@example.com>
2023-06-21 14:38:55 -0700

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,23 +8,20 @@
 
 int print_sign(int n)
 {
-
-
 	if (n > 0)
 	{
 		_putchar ('+');
 		return (1);
 	}
-	else if (n = 0)
+	else if (n < 0)
 	{
-		_putchar ('0');
-		return (0);
+		_putchar ('-');
+		return (-1);
 	}
 	else
 	{
-		_putcar ('-');
-		return (-1);
+		_putchar (0);
+		return (0);
 	}
-	return (0);
 }
 
