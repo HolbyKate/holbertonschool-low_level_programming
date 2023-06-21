3ea1df6dde635c88e6fbefb7b6815deee6da15eb
root <you@example.com>
2023-06-21 08:30:02 -0700

    (alphax10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10 - prints 10 times the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+
+int i;
+char alphabet;
+for (i = 1 ; <= 10; alphabet = 'a'; alphabet <= 'z'; alphabet++; i++)
+{
+	_putchar(alphabet);
+}
+_putchar('\n');
+}
