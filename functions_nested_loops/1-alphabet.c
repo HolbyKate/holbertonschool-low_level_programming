e702374c9f88a349fb4cae5cd919c9678543acf6
root <you@example.com>
2023-06-21 07:38:41 -0700

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet, in lowercase
- *
- * Return: Always 0.
+ * print_alphabet - prints the alphabet, in lowercase
  */
 
 void print_alphabet(void)
-
 {
 	char alphabet;
 
