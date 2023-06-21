6b4995ebc1e1d96400f047145d94ab0015d277d5
root <you@example.com>
2023-06-21 14:54:14 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - abs
+ *
+ * Return: -int if <0 int if else 
+ */
+
+int _abs(int)
+{
+	if (int < 0)
+	{
+		return (-int);
+	}
+else
+	{
+		return (int);
+	}
+}
+
