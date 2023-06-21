b87e3e9899f7a64f7ed5ab5be6f0ce069631918c
root <you@example.com>
2023-06-21 06:03:18 -0700

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,7 +8,15 @@
 int main(void)
 {
 
-	printf("_ putchar");
 
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	
 		return (0);
 }
