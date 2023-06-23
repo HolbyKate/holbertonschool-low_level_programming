f4547353298b7d1ca77ddf7209c8863088d5343f
root <you@example.com>
2023-06-23 07:38:27 -0700

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign- print sign
- * @n:Prototype: int print_sign(int n);
+ * @n: Prototype: int print_sign(int n);
  * Return: 1 if >0, 0 if =0 and -1 if <0
  */
 
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar ('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar ('-');
+		_putchar(45);
 		return (-1);
 	}
 	else
 	{
-		_putchar (0);
+		_putchar(48);
 		return (0);
 	}
 }
