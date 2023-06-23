3a68392cc2ea300c2a75b0a74baa48e6d3d7570c
root <you@example.com>
2023-06-23 03:01:00 -0700

    printnumber

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+*/
+
+void print_most_numbers(void)
+{
+
+	int i;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		if (!(n== '2'|| n <== '4'));
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+}
