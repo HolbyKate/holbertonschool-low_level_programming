aadad38406b5993c5303360fb408c2c7d220e32b
root <you@example.com>
2023-06-23 13:09:52 -0700

    timetable

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,27 +5,27 @@
  * Return: Always success
  */
 void times_table(void)
-
-	int h;
-	int L;
-	int es;
-
 {
+	int h, L, es;
+
+
 	for (h = 0; h <= 9; h++)
 	{
 		_putchar('0');
 	}
-		for (L = 1; L < 10; L++)
+	for (L = 1; L < 10; L++)
+	{
+		_putchar(44);
+		_putchar(32);
 		{
-			_putchar(44);
-			_putchar(32);
-
 			if (es / 10 == 0)
 				_putchar(32);
-
-			else
-				_putchar(es / 10 + '0');
+		}
+		else
+		{
+			_putchar(es / 10 + '0');
 			_putchar(es %  10 + '0');
 			_putchar('\n');
 		}
 	}
+}
