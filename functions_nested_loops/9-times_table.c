f3c238cc2792fbd428e8e434e2e6e348e18c5b04
root <you@example.com>
2023-06-23 08:31:31 -0700

    timestable

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (L = 1; L < / 0; L++)
+		for (L = 1; L < 10; L++)
 		{
 			_putchar(44);
 			_putchar(32);
@@ -27,7 +27,7 @@ void times_table(void)
 
 			else
 				_putchar(es / 10 + '0');
-			_putchar(es % / 10 + '0');
+			_putchar(es %  10 + '0');
 			_putchar('\n');
 		}
 	}
