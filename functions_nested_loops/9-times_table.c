37a4cf025e78792ba8e4f68c7c981d6c096c8e34
root <you@example.com>
2023-06-23 08:24:15 -0700

    timestable

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table starting with 0
+ * @h:hauteur
+ * @L: largeur
+ * @es: espace
+ * Return: Always success
+ */
+void times_table(void)
+{
+	int h;
+	int L;
+	int es;
+
+	for (h = 0; L <= 9; h++)
+	{
+		_putchar('0');
+
+		for (L = 1; L < / 0; L++)
+		{
+			_putchar(44);
+			_putchar(32);
+
+			if (es / 10 == 0)
+				_putchar(32);
+
+			else
+				_putchar(es / 10 + '0');
+			_putchar(es % / 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
