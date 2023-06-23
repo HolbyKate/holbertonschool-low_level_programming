0e75b8795f7614d4be0909830b67c38a9ca698b4
root <you@example.com>
2023-06-23 01:38:30 -0700

    printnumber

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * @n: 0 to 9
+ * Return: Always success
+ */
+void print_ numbers(void)
+{
+	int n;
+	
+	for (n >= 48 && n <= 9; n++);
+	{
+	putchar(n);
+
+	putchar(10);
+
+			
+	}
+}
