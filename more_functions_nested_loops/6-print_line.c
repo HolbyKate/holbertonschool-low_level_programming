c193a12e2a9fcd54d3b13d374fb6fb4738a7355f
root <you@example.com>
2023-06-23 13:34:04 -0700

    printline

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_line - prints traight line
+* @n: number of times the character should be printed
+*
+* Return: always success
+*/
+
+void print_line(int n)
+
+{
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+	for (j = 0; J <n; j++)
+	{
+		_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
