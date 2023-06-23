1e4df228ca4b8e53b50bd76638bbaabe96ca1c1f
root <you@example.com>
2023-06-23 03:17:04 -0700

    mostnumbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,19 +2,19 @@
 
 /**
  * print_most_numbers - print numbers
-*/
+ */
 
 void print_most_numbers(void)
 {
 
 	int n;
 
-	for (n = 0; n <= 9; ++n)
+	for (n = '0'; n <= '9'; ++n)
 	{
-		if (!(n == '2' || n == '4'))
+		if (n != '2' && n! = '4')
 		{
 			_putchar(n);
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
