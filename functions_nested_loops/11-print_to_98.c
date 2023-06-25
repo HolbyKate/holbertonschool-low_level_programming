7b6d4640ededc80da624460c7641116a59c02fa9
root <you@example.com>
2023-06-25 13:28:26 -0700

    print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <main.h>
+
+/**
+ * print_to_98 : prints all natural numbers from n to 98
+ * @n: numbers with coma and space
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	int n;
+
+	if (n <= 98; n++)
+	{
+		printf('%', n);
+
+		if (n != 98)
+		{
+			printf(44, 32);
+		}
+	}
+
+	printf('\n');
+}
+
+
