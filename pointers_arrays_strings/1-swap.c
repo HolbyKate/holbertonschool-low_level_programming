1a4260662267048e76f655b9fb4429eef5b48d99
root <you@example.com>
2023-06-26 06:35:04 -0700

    swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -5,14 +5,14 @@
  * swap_int - pointer
  * @a: updates the value
  * @b: swap value
- * @c: temp
- * Return: Always success
  */
 
 void swap_int(int *a, int b)
-
 {
-	int c = *a;
+
+	int c = 0;
+
+	c = *a;
 	*a = *b;
 	*b = c;
 }
