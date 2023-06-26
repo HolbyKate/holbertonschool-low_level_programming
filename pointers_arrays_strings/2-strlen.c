6590b19b8c709d63ca89266f3b4d7fe088e7c64d
root <you@example.com>
2023-06-26 08:00:34 -0700

    strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 
 /**
- * int _strlen - pointer
- * @i: updates the value
- * Return : always success
+ * _strlen - pointer
+ * @s: updates the value
+ * Return: always success
  */
 
 int _strlen(char *s)
 
 {
-
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
