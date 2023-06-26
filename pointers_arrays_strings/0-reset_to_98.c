dd94da2231f121ef51552c3dbcaed196ae882355
root <you@example.com>
2023-06-26 02:11:57 -0700

    reset

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - pointer
+ * @n : updates the value
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	n = 98;
+	return (0);
+}
