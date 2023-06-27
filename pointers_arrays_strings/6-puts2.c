c4c2847e3bcc55e1e4c4328531ca0a9e77a490eb
root <you@example.com>
2023-06-27 13:42:06 -0700

    puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ *
+ * Return: Always 0.
+ */
+void puts2(char *str)
+
+{
+	int = i;
+	i = 0;
+
+	if ( i >= 48 ; < 57; i++)
+	{
+		putchar (i);
+	}
+}
