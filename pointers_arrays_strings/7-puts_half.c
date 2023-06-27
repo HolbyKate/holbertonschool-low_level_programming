d14d86b1d6af00cfd93381b2fb74b21edd0e7ff3
root <you@example.com>
2023-06-27 14:17:31 -0700

    putshalf

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,13 +8,13 @@
 void puts_half(char *str)
 
 {
-        int i;
+	int i;
 
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                if (i / 2 == 0)
-                        putchar(str[i]);
-        }
-        {       putchar('\n');
-        }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i / 2 == 0)
+			putchar(str[i]);
+	}
+	{       putchar('\n');
+	}
 }
