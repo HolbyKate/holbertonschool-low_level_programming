024fd10a8c21fb11f2da37cd2acad374f12b2dad
root <you@example.com>
2023-06-27 13:05:31 -0700

    revstring

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - prints a string reverse
+ * @s : la phrase
+ * Return : void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0' ; i++)
+
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+
+	for (i -= 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
