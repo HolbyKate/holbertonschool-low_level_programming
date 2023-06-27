8761340c761de2c35505aee7d17bd2ffafe07a6b
root <you@example.com>
2023-06-27 13:12:29 -0700

    revstring

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0' ; i++)
+	for (i = 0; s[i] != '\0')
 
 	{
 		putchar(s[i]);
@@ -19,7 +19,9 @@ void rev_string(char *s)
 	putchar('\n');
 
 	for (i -= 1; i >= 0; i--)
-
+{
+                putchar(s[i]);
+        }
 		putchar('\n');
 }
 
