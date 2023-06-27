00bdcc8fb94371e75630329b1c8610274d9f364b
root <you@example.com>
2023-06-27 04:36:29 -0700

    print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string reverse
+ * @s : la phrase
+ * Return : always (0)
+ */
+
+void print_rev(char *s)
+
+{
+	int i = 0;
+
+	for (s[i] != '\0' ; i++)
+	{
+		i++;
+	}
+	i--;
+	
+	for (; i >= 0; i--);
+	{
+		_putchar (s[i]);
+	}
+	_putchar('\n');
+}
