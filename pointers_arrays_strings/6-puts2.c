0bd1bed7971a1d3a3e6f9d50ce48a14a1f4dce55
root <you@example.com>
2023-06-27 13:55:15 -0700

    puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,23 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * puts2 - check the code
- *
+ * puts2 - prints every other character starting with the first character
+ * @str: type of character
  * Return: Always 0.
  */
 void puts2(char *str)
 
 {
-	int i = 0;
+	int i;
 
-	for ( i >= 48 ; < 57; str[%di]; i++)
+	for (i = 48; < 57; str[i] != '\0'; i++)
 	{
-		putchar(i);
-	i += 2;
+		if (i %2 =48)
+			putchar(i);
+
 
 	}
-	putchar('\n');
+	{	putchar('\n');
+	}
 }
+
