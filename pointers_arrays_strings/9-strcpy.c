3d3d9aad4d6e71c8e09ad00c0bd859fd4b78a36c
root <you@example.com>
2023-06-28 04:26:38 -0700

    copies

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - copies the string pointed to by src
+ * @dest: destination
+ * @src: copies
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	int a;
+
+	for (a = 0; src[a] != '\O'; a++)
+	{
+		dest[a] = src[a];
+
+	}
+	dest[a] = '\0';
+
+	return (O);
+
+}
