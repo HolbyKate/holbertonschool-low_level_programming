a6729e1b3176d86a5477263654a2b8ab186421e8
root <you@example.com>
2023-06-28 00:49:26 -0700

    printarray

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints every other character starting with the first character
+ * @a: type of character
+ * @n: number 
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+
+{
+	int i [5];
+	int a, n;
+
+	for (a = 0; a < n; a++)
+	{
+		printf("%d\n",a [i];
+				}
+printf("\n");
+}
