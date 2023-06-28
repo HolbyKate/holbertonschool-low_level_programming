b926b43f51488e1bd22161793484bf8347178867
root <you@example.com>
2023-06-28 13:07:39 -0700

    string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,18 +9,19 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
+	int i = 0, a = 0, b;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		putchar(s[i]);
+		i++;
+	}
+	i--;
+	while (i > a)
+	{
+		b = s[i];
+		s[i--] = s[a];
+		s[a++] = b;
+
 	}
 	putchar('\n');
-
-	for (i -= 1; i >= 0; i--)
-{
-                putchar(s[i]);
-        }
-		putchar('\n');
 }
-
