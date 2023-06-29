a576eaa58c79096b8de04867f20677a9b5d2244c
root <you@example.com>
2023-06-29 12:40:00 -0700

    compare

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strncpy - copy a string
+ * _strcmp - copy a string
  * @s1: strig 1 to compare
  * @s2: string 2 to compare
  * Return: 
@@ -13,9 +13,9 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 
 	while (s1[i] != '\0' && s2[i] !='\0')
-}
-		i++;
 	{
+		i++;
+	}
 		return (s1[i], s2 [i]);
 
 	}
