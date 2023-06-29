2447a90c45d7649e63395724219de9b1871a2679
root <you@example.com>
2023-06-29 04:30:49 -0700

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - print a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number
+ * Return: always success
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		dest [i+j] = src[j];
+	}
+	return (dest);
+}
