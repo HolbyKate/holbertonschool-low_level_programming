ee0d7d682d259b9b8cd28c85efa4724fa72a4fa8
root <you@example.com>
2023-06-29 13:54:06 -0700

    reverse

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - function that prints a string
+ * @a: string to be reversed
+ * @n: number of care  of the array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, r, j;
+
+	i = 0;
+	r = n-1;
+
+	while (i<= r)
+	{
+		j = a[i];
+		a[i] = a[r];
+		a[r] = j;
+		i++;
+		r--;
+	}
+}
