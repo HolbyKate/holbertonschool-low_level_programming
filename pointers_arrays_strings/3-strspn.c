ec625b1a8f5f3414b59b955e5db8b3d452a6b7c7
root <you@example.com>
2023-06-30 07:35:49 -0700

    strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - gets the length
+ *@s: first segment
+ *@accept: second string to analyse
+ * Return: void
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	int count = 0;
+
+	while (s[i] != '\0')
+
+	{
+		for (j = 0 ; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				breack;
+			}
+		}
+
+		if (accept[j] == '\0')
+		{
+			return (count);
+		}
+		i++;
+	}
+	return (count);
+}
