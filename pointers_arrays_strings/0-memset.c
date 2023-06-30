2730769d7f2345bad05daed8d551b43789f6a957
root <you@example.com>
2023-06-30 02:46:00 -0700

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - fills memory
+ * @s: memory area
+ * @b: constant byte
+ * @n: unsigned bytes
+ *
+ * Return: Nothing.
+ */
+
+= 0; ichar *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	ifor (i = 0; i < n; i++)
+	{s[i] = b;
+	}
+	return (0)
+}
