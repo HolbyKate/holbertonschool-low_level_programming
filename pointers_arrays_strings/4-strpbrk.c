56ed4b2734e1ba97f925209a83d752c3d9a77c4a
root <you@example.com>
2023-06-30 13:13:40 -0700

    search

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -13,16 +13,20 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i = 0;
 	int j = 0;
+	
 	while (s[i] != '\0')
 	{
-		j= 0;
+		j = 0;
 
-		while (accept [j]!= '\0')
+		while (accept[j] != '\0')
 		{
-			return(0);
+			if (s[i] == accept[j])
+
+				return (s + i);
 
 			j++;
 		}
+		i++;
 	}
-	return(NULL);
+	return (NULL);
 }
