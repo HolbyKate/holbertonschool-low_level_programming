8a02f529d35a173f9d67b1dd7608f8802a13cb8b
root <you@example.com>
2023-06-30 01:14:26 -0700

    prints

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -15,13 +15,13 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 
-	while (i != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
 	j = 0;
-	while (j != '\0')
+	while (dest[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
