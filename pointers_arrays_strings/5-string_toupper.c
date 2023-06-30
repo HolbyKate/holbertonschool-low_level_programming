39ba25875071b1aac2499093a33d291200ca38f3
root <you@example.com>
2023-06-30 00:46:07 -0700

    upper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - function that changes all lowercase letters of a string
+ * @s: changer string
+ * Return: void
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+	i = 0;
+
+	while (s[i] ! = '\0')
+
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+
+			s[i] -= 32;
+	}
+}
+printf("%s\n", s);
+return s;
+}
