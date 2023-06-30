e3a6618b3fc897db787578d53aac9542dd204f4b
root <you@example.com>
2023-06-30 00:51:19 -0700

    upper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -10,16 +10,16 @@
 char *string_toupper(char *s)
 {
 	int i;
+
 	i = 0;
 
-	while (s[i] ! = '\0')
+	while (s[i] != '\0')
 
 	{
 		if (s[i] >= 97 && s[i] <= 122)
 
 			s[i] -= 32;
-	}
 }
-printf("%s\n", s);
-return s;
+
+return (s);
 }
