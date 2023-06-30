8c08462c183543fb09f1083237fcb59f5e16c377
root <you@example.com>
2023-06-30 05:20:53 -0700

    memcopy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,5 +11,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int n;
+	unsigned int n = i;
 
+	while (i = 0 ; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
