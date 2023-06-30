72594b4db052f26f109d02553ba3c67454484286
root <you@example.com>
2023-06-30 05:13:30 -0700

    memcopy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy : function that copies memory area
+ * @dest: type char pointer
+ * @src: type char memory area
+ * @n: bytes
+ * Return: dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int n;
+
