e53e1a656347fd541bc81d907292d17e7562b634
root <you@example.com>
2023-06-30 13:03:43 -0700

    search

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - check the code
+ * @s: string where is the first occurrence
+ * @accept: string of result
+ *
+ * Return: Always 0.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	while (s[i] != '\0')
+	{
+		j= 0;
+
+		while (accept [j]!= '\0')
+		{
+			return(0);
+
+			J++;
+		}
+	}
+	return(NULL);
+}
