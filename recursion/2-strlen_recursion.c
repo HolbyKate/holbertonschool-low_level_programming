c57f5476ca3031b45ce76f32a420acd881b2b6bc
root <you@example.com>
2023-07-05 03:10:09 -0700

    strlenrecursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,15 +5,14 @@
  * _strlen_recursion - returns the length of a string
  * @s: string to length
  *
- * Return (0)
+ * Return: (0)
  */
 
 int _strlen_recursion(char *s)
-
 {
 	if (*s != '\0')
 	{
-		return (1 + _strlen_recursion (s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 	return (0);
 }
