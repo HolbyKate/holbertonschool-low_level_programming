70e8304529d8f5c09cde9d1e7e2223106cf0c468
root <you@example.com>
2023-07-06 14:31:25 -0700

    readme

---
+++ 0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: string whith recursion
+ *
+ * Return : Always 0.
+ */
+void _puts_recursion(char *s)
+
+{
+	int c = 0;
+
+	    if (s[c] == '\0');
+
+	putchar ('\n');
+
+	return(0);
+}
