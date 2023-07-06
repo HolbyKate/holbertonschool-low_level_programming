85ad4828d32f8fed0e84c3889ce9bc9eafa0e936
root <you@example.com>
2023-07-06 14:29:32 -0700

    whatmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: the size
+ * @argv: the array
+ *
+ * Return: always success
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	putchar (argv[0]);
+	return (0);
+}
