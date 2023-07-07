990c3fe141b35fa10ede093b452dcebd3d072f31
root <you@example.com>
2023-07-07 01:27:38 -0700

    myname

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
  * main - prints the name file, followed by a new line
- * @argc: the size
+ * @argc: the number of argurment
  * @argv: the array
  *
  * Return: name file
  */
 
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
