a3c93fbf16ec30786793a201f581b145fb264f92
root <you@example.com>
2023-07-07 08:14:05 -0700

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number type
+ * @argv: print number
+ *
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int sum = 0;
+
+
+	for (j = 1 ; j < argc ; j++)
+	{
+		char *arg = argv[j];
+		int isDigit = 1;
+
+		for (i = 0 ; arg[i] != '\0' ; i++)
+		{
+			if (!isdigit(arg[i]))
+			{
+				isDigit = 0;
+				break;
+			}
+		}
+		if (isDigit)
+		{
+			sum += atoi(arg);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
