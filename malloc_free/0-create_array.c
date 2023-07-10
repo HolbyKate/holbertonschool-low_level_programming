0a7e61f89f617de21eb329a00e0fe86446839a5b
root <you@example.com>
2023-07-10 07:35:15 -0700

    array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,16 +12,17 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *s;
 	unsigned int i;
-	char *s = NULL;
+
+	if (size == 0)
+		return (NULL);
 
 	s = malloc(size * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 
-	if (size == 0)
-		return (NULL);
-
+	i = 0;
 	while (i < size)
 	{
 		s[i] = c;
