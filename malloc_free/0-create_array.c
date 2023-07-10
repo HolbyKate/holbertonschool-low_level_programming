c3d01c392fba0f358e0f6dde09f2e655ca099ff8
root <you@example.com>
2023-07-10 06:57:37 -0700

    array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -7,27 +7,26 @@
  * @size: size for the memories
  * @c: characters
  * @s: size
- * Return: always 0.
+ * Return: pointer to the array or size, or NULL if it fails
  */
 
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-
-	i = 0;
 	char *s;
 
-	if (char == NULL)
+	s = malloc(size * sizeof(char));
+	if (s == NULL)
 		return (NULL);
 
 	if (size == 0)
 		return (NULL);
 
 	for (i <= size)
-	
-		char[i] = c;
-		i++;
-	
+
+		s[i] = c;
+	i++;
+
 	return (s);
 }
 
