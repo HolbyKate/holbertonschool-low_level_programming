fdaca506e5311c311cfee2d7c06c8a8cf26a6ef6
root <you@example.com>
2023-07-12 05:36:49 -0700

    allocgrid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @w: largeur
+ * @h: hauteur
+ * 
+ * Return: a pointer or NULL on failure, 0 or negative
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid
+	int w = 0;
+	int h = 0;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	grid = (int **)malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
+
+	for (h = 0; h < height; h++)
+	{
+		grid[h] = (int *)malloc(width * sizeof(int));
+
+		if (!grid[h])
+		{
+			while (h)
+			{
+				free(grid[h]);
+				h--;
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (w = 0; w < width; w++)
+			grid[h][w] = 0;
+	}
+
+	return (grid);
+}
