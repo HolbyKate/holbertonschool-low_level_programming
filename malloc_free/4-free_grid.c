70b8dc2e7b634e0b807155e314ef05895b9650c4
root <you@example.com>
2023-07-12 07:15:13 -0700

    freegrid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * free_grid - frees a 2 dimensional grid previously
+ * @grid: grille
+ * @height: largeur
+ *
+ */
+
+void free_grid(int **grid, int height)
+
+{
+	int i = 0;
+
+	if (grid == NULL)
+		free(grid);
+
+	if (i < height)
+	{
+		free(grid[i]);
+				i++;}
+}
