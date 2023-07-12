b4ddf4707be3405352309bcb748531843c9c83d6
root <you@example.com>
2023-07-12 02:57:07 -0700

    concatstr

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,16 +19,15 @@ char *str_concat(char *s1, char *s2)
 	if (s1 != NULL)
 		for (i = 0; s1[i]; i++)
 
+			if (s1 == NULL)
+				s1 = "";
+	if (s2 != NULL)
+		for (j = 0; s2[j]; j++)
 
-			if (s2 != NULL)
-				for (j = 0; s2[j]; j++)
+			if (s2 == NULL)
+				s2 = "";
 
-if (s1 == NULL)
-        s1 = "";
-if (s2 == NULL)
-        s2 = "";
-
-s = malloc(sizeof(char) * (i + j + 1));
+	s = malloc(sizeof(char) * (i + j + 1));
 
 	if (s == NULL)
 		return (NULL);
