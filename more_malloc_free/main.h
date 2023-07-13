44c68f596cf2a2e62f66695968887b3900e7e796
root <you@example.com>
2023-07-13 00:54:05 -0700

    main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,7 +1,4 @@
 #ifndef MAIN.H
 #define MAIN.H
 void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
 #endif
