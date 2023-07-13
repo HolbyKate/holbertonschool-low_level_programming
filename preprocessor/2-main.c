99d854b27e9eaf8349ce86359ae7bf2fb08e2d9c
root <you@example.com>
2023-07-13 05:34:13 -0700

    main.c

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the name of the file follow by a new line
+ *
+ * Return: success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
