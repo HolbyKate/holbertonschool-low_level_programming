96bb706828dda36bd9cb74b5300f43fc8f1f866f
root <you@example.com>
2023-07-14 01:07:33 -0700

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *pointer;
 	unsigned int i;
-	unsigned int calloc = nmemb * size
+	unsigned int calloc = nmemb * size;
 
 		if (nmemb == 0 || size == 0)
 			return (NULL);
