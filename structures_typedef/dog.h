14c578c79a53f223e3941d9f53246a8a2470866b
root <you@example.com>
2023-07-17 02:29:42 -0700

    structure dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure pour Poppy
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: définition d'un new type
+ */
 struct dog
 {
-        char* name;
-        float age;
-        char* owner;
+	char *name;
+	float age;
+	char *owner;
 };
+#endif
