acf261d07c0d6528bc85073753f3f39065e1f828
root <you@example.com>
2023-07-18 05:37:55 -0700

    newdog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include <string.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * @toutou: le d pour toutou
+ * Return: NULL if function fails,
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *toutou;
+	int a, b;
+
+
+	for (a = 0; name[a]; a++)
+		;
+	for (b = 0; owner[b]; b++)
+		;
+	/* j'alloue de la mémoire à toutou */
+	toutou = malloc(sizeof(dog_t));
+	if (toutou == NULL)
+		return (NULL);
+	/* j'alloue de la mémoire à .name & .owner. Si null je free  mémoire */
+	toutou->name = malloc(a + 1);
+	if (toutou->name == NULL)
+	{
+		free(toutou);
+		return (NULL);
+	}
+	toutou->owner = malloc(b + 1);
+	if (toutou->owner == NULL)
+	{
+		free(toutou->name);
+		free(toutou);
+		return (NULL);
+	}
+	/* je copie les deux éléments demandés */
+	strcpy(toutou->name, name);
+	strcpy(toutou->owner, owner);
+	toutou->age = age;
+	return (toutou);
+}
