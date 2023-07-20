0587bf6a7514f1fecaf6887dffce63804f0a2491
root <you@example.com>
2023-07-20 04:58:38 -0700

    main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,28 +12,31 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2;
-	char *op
-	
+	char *op;
+
 	if (argc !=4)
 	{
 		printf("Error \n");
 		exit(98);
 	}
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	op = argv[2];
-	if ((argv(2, '/') || argv(2, '%')) && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	if (!(get_op_func(op)))
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = (get_op_func(op))(num1, num2);
-	printf("%d\n", result);
+
+	if ((*op == '\' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op func(op)(num1, num2));
 
 	return (0);
 }
