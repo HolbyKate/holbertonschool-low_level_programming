48ef9a1a0c18540e1198a796308bbf04bf9ccfd8
root <you@example.com>
2023-07-21 13:29:22 -0700

    printstring

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -8,35 +8,32 @@
  * @n: number of strings passed to be function
  * @...: list
  *
- * Return void
+ * Return nothing
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-
 	va_list ap;
 	unsigned int i = 0;
 	char *string;
 
-	if (n > 0)
+	va_start(ap, n);
+
+	while (i < n)
 	{
-		va_start(ap, n);
+		string = va_arg(ap, char*);
+		if (string == NULL)
+			printf("(nil)");
+		else
+			printf("%s", string);
 
-		while (i < n)
-		{
-			string = va_arg(ap, char*);
-			if (string == NULL)
-				printf("(nil)");
-			else
-				printf("%s", string);
-
-			if (i != n - 1 && separator != NULL)
-				printf("%s", separator);
-			i++;
-		}
-
-		va_end(ap);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+		i++;
 	}
+
+	va_end(ap);
+
 	printf("\n");
 }
 
