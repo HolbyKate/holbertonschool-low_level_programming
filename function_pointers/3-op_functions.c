6ac9f72d40e78ee09d592adb8b4c23747159b425
root <you@example.com>
2023-07-21 13:05:35 -0700

    opfunction

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -15,15 +15,39 @@ int op_add(int a, int b)
 	return (a + b);
 }
 
+/**
+ * op_sub - return the sustraction of a and b
+ * @a: first integer
+ * @b: seond integer
+ *
+ * Return: the result of sum
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - return the mul of a and b
+ * @a: first integer
+ * @b: seond integer
+ *
+ * Return: the result of sum
+ */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
+/**
+ * op_div - return the div of a and b
+ * @a: first integer
+ * @b: seond integer
+ *
+ * Return: the result of sum
+ */
 
 int op_div(int a, int b)
 {
@@ -34,6 +58,14 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+
+/**
+ * op_mod - return the mod of a and b
+ * @a: first integer
+ * @b: seond integer
+ *
+ * Return: the result of sum
+ */
 
 int op_mod(int a, int b)
 {
