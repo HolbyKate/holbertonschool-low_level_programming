6dd44a36ec94e215820c4f72063a84230c794397
root <you@example.com>
2023-07-28 05:50:13 -0700

    fix betty style

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,21 +1,21 @@
+#include "dog.h"
 #include <string.h>
 #include <stdlib.h>
-#include "dog.h"
 
 /**
  * new_dog - function that creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- * @toutou: le d pour toutou
- * Return: NULL if function fails,
+ * @toutou: the new dog
+ *
+ * Return: pointer to dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *toutou;
 	int a, b;
-
 
 	for (a = 0; name[a]; a++)
 		;
