26431e17703364b9c072237b4ac0bc3f7465221c
root <you@example.com>
2023-08-01 02:50:59 -0700

    listlen

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: pointer to string
+ *
+ * Return: always 0
+ */
+
+size_t print_list(const list_t *h)
+{
+        size_t count = 0;
+        const list_t *temp = h;
+
+        for (count = 0; temp; count++)
+	{        return (count);
+}
+}
