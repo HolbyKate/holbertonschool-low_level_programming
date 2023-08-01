781d8bce07c2a2014ff085737aab299790dc0877
root <you@example.com>
2023-08-01 12:29:42 -0700

    addnode

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,11 +12,10 @@
  */
 
 list_t *add_node(list_t **head, const char *str)
-
 {
 	list_t *new_node = NULL;
-	new_node = malloc(sizeof(list_t));
 
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		free(new_node);
