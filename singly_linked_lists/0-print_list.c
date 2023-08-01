ff3cf6139c6cb7f6871d34b0be9a337f9bbbfd3f
root <you@example.com>
2023-08-01 02:24:19 -0700

    printlists

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@
  * print_list - function that prints all the elements of a list_t list
  * @h: pointer to string
  *
- * Return: count
+ * Return: always 0
  */
 
 size_t print_list(const list_t *h)
