f7bad117a63e6fd12ff31cae76e88b52a1d387f3
root <you@example.com>
2023-08-01 03:26:28 -0700

    listlen

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -7,20 +7,18 @@
  * print_list - function that prints all the elements of a list_t list
  * @h: pointer to a linked list -t
  *
- * Return: always 0
+ * Return: number of elements
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	int count = 0;
 
-	for (count = 0; h != NULL)
+	if (h == NULL)
 		return (count);
-
-	while (h != NULL)
-		count++;
-	{
+	for (; h != NULL; count++)
+	{	
 		h = h->next;
-		return (count);
-	}
+	}	
+	return (count);
 }
