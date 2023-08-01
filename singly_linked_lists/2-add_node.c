3d7e51746415270bd4e1966f8561458f44db8067
root <you@example.com>
2023-08-01 06:17:51 -0700

    newnode

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -6,24 +6,26 @@
 /**
  * add_node - function that adds a new node at the beginning of a list_t list
  * @str: string for new node
+ * @head: the new_node
  *
  * Return: he address of the new element
  */
 
 list_t *add_node(list_t **head, const char *str)
+
 {
 	list_t *new_node = NULL;
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 	{
-		free (new_node);
+		free(new_node);
 		new_node = NULL;
 		return (NULL);
 	}
-	new_node -> str = strdup (str);
-	new_node -> len = strlen (str);
-	new_node -> next =  *head;
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next =  *head;
 	*head = new_node;
 	return (*head);
 }
