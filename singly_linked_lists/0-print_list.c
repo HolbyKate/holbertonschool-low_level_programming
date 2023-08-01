70bbd9e4548de38bb88c70581ff5390d45781be9
root <you@example.com>
2023-08-01 02:12:16 -0700

    printlists

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,17 +15,14 @@ size_t print_list(const list_t *h)
 	size_t count = 0;
 	const list_t *temp = h;
 
-	while (temp == NULL)
+	while (count == 0 || temp == NULL)
 		count++;
 	{
 		if (temp->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
+
 		else
-		{
-			printf("[%d], %s", temp->len, temp->str);
-		}
+			printf("[%d], %s\n", temp->len, temp->str);
 		count++;
 		temp = temp->next;
 	}
