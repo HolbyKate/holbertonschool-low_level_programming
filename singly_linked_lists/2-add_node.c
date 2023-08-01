78a6ef80387521a7edd0de4d0f40c61d245017c0
root <you@example.com>
2023-08-01 06:11:41 -0700

    newnode

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -18,12 +18,12 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 	{
 		free (new_node);
-		new_node == NULL;
+		new_node = NULL;
 		return (NULL);
 	}
-	new_node => str = strdup (str);
-	new_node => len = str (str);
-	new_node => *head;
+	new_node -> str = strdup (str);
+	new_node -> len = strlen (str);
+	new_node -> next =  *head;
 	*head = new_node;
 	return (*head);
 }
