0df6ff1f5985c7f773dad2c5cf694c10bbc09c88
root <you@example.com>
2023-08-01 00:24:40 -0700

    printlist

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @list: list the 
+ * @h: pointer to 
+ *
+ * Return: 0 if fail
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *temp = h;
+
+	for (count = 0; temp; count++)
+		size_t count = 0;
+	const list_t *temp = h;
+
+	for (count = 0; temp; count++)
+	{
+		if (temp->str == NULL)
+			printf("[0] (nil)\n");
+	}
+	return (count);
+}
