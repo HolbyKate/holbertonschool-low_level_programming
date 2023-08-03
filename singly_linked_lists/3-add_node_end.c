059871546382688f7c9e1324f045546a6323465a
root <you@example.com>
2023-08-03 05:56:55 -0700

    fix the structure to compilate

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -18,17 +18,15 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
-		free(new_node);
-		new_node = NULL;
 		return (NULL);
 	}
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+	new_node->len = strlen(str);
 	new_node->next = NULL;
+
+	if (new_node->str == NULL)
+		new_node->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
