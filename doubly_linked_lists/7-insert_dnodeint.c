d8e53f443a3d39e240bbb9c8db628f80ee06c862
root <you@example.com>
2023-08-03 13:28:50 -0700

    fix zero and malloc line+ kernel in insert_dnodeint

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - function that inserts 
+ * insert_dnodeint_at_index - function that inserts
  * a new node at a given position
  * @h: pointer to head
  * @idx: index of the list where the new node should be added
