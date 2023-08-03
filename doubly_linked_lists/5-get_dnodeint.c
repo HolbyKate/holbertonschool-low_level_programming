a7c2071416f75571c682320c1eb8e48b8b6a8290
root <you@example.com>
2023-08-03 05:15:25 -0700

    fix the segmentation fault

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,20 +13,21 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
+	dlistint_t *h = head;
 
 	if (head == NULL)
-		return (NULL);
-
-	while (head->prev != NULL)
-		head = head->prev;
-
-	i = 0;
-	while (head != NULL)
 	{
-		head = head->next;
+		return (NULL);
+	}
+	while (h != NULL)
+	{
+		if (i == index)
+		{
+			return (h);
+		}
+		h = h->next;
 		i++;
 	}
-
-	return (head);
+	return (h);
 }
