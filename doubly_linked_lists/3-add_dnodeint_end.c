1789757384e2016c4db2ce27844d9152016fe7ba
root <you@example.com>
2023-08-03 05:41:58 -0700

    fix structure of code

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -22,8 +22,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
-		free(new_node);
-		new_node = NULL;
 		return (NULL);
 	}
 	new_node->n = n;
@@ -31,13 +29,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->next = NULL;
 	if (*head == NULL)
 	{
-		free(new_node);
-		return (NULL);
 		*head = new_node;
 	}
 	else
 	{
-
+		current = *head;
 		while (current->next != NULL)
 		{
 			current = current->next;
