04b945194da8cc78eabc99dbd1335fc2e8004ee1
root <you@example.com>
2023-08-03 05:22:07 -0700

    fix kernel style

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,11 +4,11 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - function that returns the nth node of a dlistint_t linked list
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
  * @head: head of the list
  * @index: index of the list
  *
- * Return : the nth node of a dlistint
+ * Return: the nth node of a dlistint
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
