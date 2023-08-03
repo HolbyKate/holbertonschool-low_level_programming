c436632bc1a2f0d4fd25c6f4789ce9c7396a0eca
root <you@example.com>
2023-08-03 03:20:21 -0700

    fix parentesis for the pointer

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,18 +13,20 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = NULL;
+	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
-		new_node = NULL;
 		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if ((*head) != NULL)
+	{
+		(*head)->prev = new_node;
 	}
-		new_node->n = n;
-		new_node->prev = NULL;
-		new_node->next = NULL;
-		*head = new_node;
-		return (new_node);
+	*head = new_node;
+	return (new_node);
 }
