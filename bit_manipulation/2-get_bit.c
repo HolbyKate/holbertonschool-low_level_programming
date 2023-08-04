0af22c653a1d9a9fca19a1bd43132d4f52d46aac
root <you@example.com>
2023-08-04 01:03:17 -0700

    getbit

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,13 +9,13 @@
  *
  * Return: the value of the bit at index index or -1 if an error occured
  */
-	i
+
 int get_bit(unsigned long int n, unsigned int index)
 
 {
 	int bit;
 
-	bit = ((n >> index) & 1);
+	bit=((n >> index) & 1);
 	if (index > 64)
 		return (-1);
 	return (bit);
