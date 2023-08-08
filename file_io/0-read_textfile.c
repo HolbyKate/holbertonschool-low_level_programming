c349bdc4025c343d316a171a8da39fd06c66c0c6
root <you@example.com>
2023-08-08 05:57:17 -0700

    fix structure in main.h to reconize the open function

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
@@ -14,39 +12,32 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes_read, bytes_write;
+	ssize_t fd;
+	ssize_t bytes_read;
+	ssize_t bytes_write;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	/*open the file*/
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	/*size malloc, number of letters*/
-	buffer = malloc(letters * sizeof(char));
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	/*read the expected amount of bytes*/
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
 		return (0);
-	}
-	/*write the expected amount of bytes*/
+
 	bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
-	/*if write fails or does not write the expected amount of bytes, return 0*/
-	if (bytes_write == -1 || != bytes_read)
-	{
-		free(buffer);
-		close(fd);
+	if (bytes_write == -1)
 		return (0);
-	}
+
+	free(buffer);
 	close(fd);
-	return bytes_write;
+	return (bytes_write);
 }
+
