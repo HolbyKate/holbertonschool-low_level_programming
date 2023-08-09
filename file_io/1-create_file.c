b407314f0dbd757a2f8e272009ea80eefc0f3f94
root <you@example.com>
2023-08-09 02:13:35 -0700

    fix texte_content is null

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,16 +14,29 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int nbytes_write;
 
 	if (filename == NULL)
+	{
 		return (-1);
-
-	fd = open(filename, O_RDONLY, 0_WRONLY);
+	}
+	fd = open(filename, O_RDONLY, O_WRONLY, O_CREAT, O_TRUNC);
 	if (fd == -1)
+	{
 		return (-1);
-
+	}
 	if (text_content == NULL)
+	{
 		close (fd);
-	return (1);
-
+		return (1);
+	}
+	else 
+	{		ssize_t len = strlen(text_content);
+		ssize_t bytes_write = write(fd, text_content, len);
+		if (bytes_write != len)
+		{
+			close(fd);
+			return -1;
+		}
+	}
+return (1);
+}
