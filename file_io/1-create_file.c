6d5dd04e1306ed82a1588474128b210cbf9d4b24
root <you@example.com>
2023-08-09 02:25:59 -0700

    fix betty style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_RDONLY | O_WRONLY | O_CREAT | O_TRUNC);
+	fd = open(filename | O_WRONLY | O_CREAT | O_TRUNC);
 	if (fd == -1)
 	{
 		return (-1);
