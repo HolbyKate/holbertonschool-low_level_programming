8185cf60c6e98ef4baec285c3a2eb94848838fde
root <you@example.com>
2023-08-09 02:20:59 -0700

    fix betty style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,16 +26,18 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		close (fd);
+		close(fd);
 		return (1);
 	}
-	else 
-	{		ssize_t len = strlen(text_content);
+	else
+	{
+		ssize_t len = strlen(text_content);
 		ssize_t bytes_write = write(fd, text_content, len);
+
 		if (bytes_write != len)
 		{
 			close(fd);
-			return -1;
+			return (-1);
 		}
 	}
 return (1);
