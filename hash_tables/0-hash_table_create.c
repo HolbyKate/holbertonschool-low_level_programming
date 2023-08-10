93c08627b2ca76c9ae45f24bc51a4ad31300a965
root <you@example.com>
2023-08-10 02:38:28 -0700

    fix_arraymode

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,24 +11,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	hash_node_t *array;
-	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
+	
 	if (ht == NULL)
 	{
 		return (NULL);
 	}
 	ht->size = size;
-	ht->array = malloc(size, sizeof(hash_node_t *));
-	
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (ht->array == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
-	for (i = 0; i< size; i++)
-	{
-		return (ht);
-	}
+	return (ht);
 }
