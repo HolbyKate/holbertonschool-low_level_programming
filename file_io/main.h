5d668e2e8778cf37772a5020e22c3a7ffa2c56d1
root <you@example.com>
2023-08-11 06:51:42 -0700

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -12,4 +12,11 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _strlen(char *s)
+{
+	int c = 0;
+	while (s[c])
+		c++;
+	return (c);
+}
 #endif
