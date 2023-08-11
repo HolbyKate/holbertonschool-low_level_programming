6588f76c03f525268193b16001bc86b012d31927
root <you@example.com>
2023-08-11 07:11:37 -0700

    fix text_content

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -31,8 +32,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return ( 1);
 	}
 	
-	bytes_write = write(fd, text_content, _strlen(text_content));
-	if (bytes_write == -1 || bytes_write != _strlen(text_content))
+	bytes_write = write(fd, text_content, strlen(text_content));
+	if (bytes_write == -1)
 	{
 		close(fd);
 		return (-1);
