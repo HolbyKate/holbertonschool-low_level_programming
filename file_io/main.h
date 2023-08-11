ce0af9abf1151e7a34306bff2aab16cede694110
root <you@example.com>
2023-08-11 07:19:40 -0700

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -11,5 +12,4 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int _strlen(char *str);
 #endif
